00a073b1e06cac77625aa7ef493792447afd481c
SwatiSKanor <swati@thinkbridge.in>
2022-09-26 17:29:11 +0530

    code

--- OutlookMailParser_POC/Program.cs
+++ OutlookMailParser_POC/Program.cs
@@ -15,6 +15,7 @@ using System.Security;
 using CSOMDemo;
 using System.Net;
 using AuthenticationManager = CSOMDemo.AuthenticationManager;
+using System.Threading.Tasks;
 
 namespace OutlookMailParser_POC
 {
@@ -26,7 +27,7 @@ namespace OutlookMailParser_POC
 
         static async System.Threading.Tasks.Task Main(string[] args)
             {
-
+          //  CreateNewFolderSharePoint();
             // Using Microsoft.Identity.Client 4.22.0  
             // thinkbridge active directory - Shared inbox outlook poc 
             var cca = ConfidentialClientApplicationBuilder
@@ -54,8 +55,8 @@ namespace OutlookMailParser_POC
 
                 EmailMessage email = new EmailMessage(service);
 
+                ReplytoSharedInboxEmail();
 
-                
 
                 PropertySet set = new PropertySet(BasePropertySet.IdOnly, ItemSchema.Subject, ItemSchema.UniqueBody, ItemSchema.Attachments,
              EmailMessageSchema.Body, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.DateTimeReceived, EmailMessageSchema.HasAttachments); //ItemSchema.TextBody,
@@ -64,15 +65,20 @@ namespace OutlookMailParser_POC
                 // Make an EWS call
                 SearchFilter foldername = new SearchFilter.IsEqualTo(FolderSchema.DisplayName, "FastaffTravelUpdates");
 
-                var folders = service.FindFolders(WellKnownFolderName.MsgFolderRoot, foldername, new FolderView(100)).Result;
+            //    var folders = service.FindFolders(WellKnownFolderName.MsgFolderRoot, foldername, new FolderView(100)).Result;
                 SearchFilter time = new SearchFilter.IsLessThanOrEqualTo(ItemSchema.DateTimeReceived, DateTime.UtcNow);
               //  SearchFilter subjectFilter = new SearchFilter.ContainsSubstring(ItemSchema.Subject, "[EXTERNAL] License nursys Jessica Welch (369580)");
                 ItemView view = new ItemView(100);
 
-             //   var findResults = service.FindItems(WellKnownFolderName.Inbox,time, view).Result;
+                //var findResults = service.FindItems(WellKnownFolderName.Inbox,time, view).Result;
 
+                // shared inbox mail access
 
-               var findResults = folders.Folders.FirstOrDefault().FindItems(time, view).Result;
+                FolderId SharedMailbox = new FolderId(WellKnownFolderName.Inbox, "compassa@thinkbridge.in");
+                ItemView itemView = new ItemView(100);
+                FindItemsResults<Item> findResults = service.FindItems(SharedMailbox, "", itemView).Result;
+
+                //  var findResults = folders.Folders.FirstOrDefault().FindItems(time, view).Result;
                 foreach (Item item in findResults)
                 {
                     var mail = item as EmailMessage;
@@ -83,7 +89,8 @@ namespace OutlookMailParser_POC
                     var mailId = mail.Id.UniqueId;
                     var from = message.From;
                     var conversationId = item.ConversationId;
-                    
+                   
+                   
                     // Request conversation items. This results in a call to the service.
                     ConversationResponse response = service.GetConversationItems
    (conversationId, set, null, null,
@@ -128,7 +135,8 @@ namespace OutlookMailParser_POC
                             }
                         }
                     }
-                  //  message.ReplyTo.Add("priyanka@thinkbridge.in");
+                  
+                  //  await message.Reply( "Reply for test.",false);
                     #region this code belongs to attachments not received in chains.
                     //if (message.HasAttachments)
                     //{
@@ -165,6 +173,36 @@ namespace OutlookMailParser_POC
                     Console.ReadKey();
                 }
             }
+        public static async void ReplytoSharedInboxEmail()
+        {
+            var mailId = "AQMkADcwYmE1ZjFlLTFkNGMtNGUzMC04ZTA5LWU5YWY2MmIzYTI0MABGAAADfpF1NE3FOECC8Qouth0+OQcAWdx5YeH+FUegFE2qtIz+UwAAAgEMAAAAWdx5YeH+FUegFE2qtIz+UwAAAgVbAAAA";
+            PropertySet set = new PropertySet(BasePropertySet.IdOnly, ItemSchema.Subject, ItemSchema.UniqueBody, ItemSchema.Attachments,
+         EmailMessageSchema.Body, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.DateTimeReceived, EmailMessageSchema.HasAttachments); //ItemSchema.TextBody,
+
+            EmailMessage msg = (EmailMessage)Item.Bind(service, new ItemId(mailId), set).Result;
+            var subject = msg.Subject;
+            var rec = msg.From;
+            ResponseMessage responseMessage = msg.CreateReply(false);
+        
+            string myReply = "This is the message body of the email reply.";
+            responseMessage.BodyPrefix = myReply;
+           
+            responseMessage.IsDeliveryReceiptRequested = true;
+            responseMessage.ToRecipients.Add("priyanka@thinkbridge.in");
+
+            // Send the response message.
+            // This method call results in a CreateItem call to EWS.
+            try
+            {
+                await responseMessage.SendAndSaveCopy();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
 
         public static string SaveFileToBlobStorage(string fileName, string contentType, byte[] content, string storageContainer, string contentDisposition = "")
         {
@@ -218,9 +256,6 @@ namespace OutlookMailParser_POC
                 var folderPath = context.Web.GetFolderByServerRelativeUrl("Shared Documents/RPA/Shared Inbox POC");
                 context.Load(folderPath, k => k.Name, k => k.Files, k => k.Folders);
                 context.ExecuteQuery();
-                //folderPath.Folders.Add(msgId);
-                //folderPath.Update();
-                //context.ExecuteQuery();
                 Microsoft.SharePoint.Client.File uploadFile = folderPath.Files.Add(fileCreationInfo);
 
 
@@ -234,6 +269,60 @@ namespace OutlookMailParser_POC
             }
              
             return string.Empty;
+        }
+
+        public static void CreateNewFolderSharePoint()
+        {
+
+            Uri site = new Uri("https://psgglobal.sharepoint.com/sites/RPA_AdminAccounts");
+            string password = "1Jaiho!Jaiho!";
+            string user = "rpa@psgglobal.com";
+            SecureString secureString = new SecureString();
+            password.ToList().ForEach(secureString.AppendChar);
+            using (AuthenticationManager authenticationManager = new AuthenticationManager())
+            using (var context = authenticationManager.GetContext(site, user, secureString))
+            {
+
+                var today = DateTime.UtcNow.Date.ToString("MM/dd/yyyy");
+
+                var folderPath = context.Web.GetFolderByServerRelativeUrl("Shared Documents/RPA/Shared Inbox POC/"+today+"/TaskId_" +1111.ToString() );
+                try
+                {
+                    context.Load(folderPath, k => k.Name, k => k.Files, k => k.Folders);
+                    context.ExecuteQuery();
+                    if (!folderPath.Exists)
+                    {
+
+                        var taskFolder = folderPath.Folders.Add("TaskId_" + 111.ToString());
+                        context.Load(taskFolder);
+                        context.ExecuteQuery();
+                        Console.WriteLine("New folder Created");
+                    }
+                    else
+                    {
+                        context.Load(folderPath, k => k.Name, k => k.Files, k => k.Folders);
+                        Console.WriteLine("Folder already exists");
+
+                    }
+                }
+                catch(ServerException ex)
+                {
+                    var folderPath1 = context.Web.GetFolderByServerRelativeUrl("Shared Documents/RPA/Shared Inbox POC/" + today + "/");
+
+                    var taskFolder = folderPath1.Folders.Add("TaskId_" + 111.ToString());
+                    context.Load(taskFolder);
+                    context.ExecuteQuery();
+                    Console.WriteLine("New folder Created");
+
+                }
+              
+
+                //var newF =  folderPath.Folders.Add(today);
+                //context.Load(newF);
+                //context.ExecuteQuery();
+
+            }
+              
         }
     }
     }
