df651fed40bbb2573dc3f1a2ad1f53d8c4d5ae2b
SwatiSKanor <swati@thinkbridge.in>
2022-10-10 16:29:09 +0530

    poc

--- OutlookMailParser_POC/Program.cs
+++ OutlookMailParser_POC/Program.cs
@@ -16,6 +16,8 @@ using CSOMDemo;
 using System.Net;
 using AuthenticationManager = CSOMDemo.AuthenticationManager;
 using System.Threading.Tasks;
+using System.Text;
+using System.Globalization;
 
 namespace OutlookMailParser_POC
 {
@@ -57,104 +59,117 @@ namespace OutlookMailParser_POC
 
                 ReplytoSharedInboxEmail();
 
+                ExtendedPropertyDefinition replyEmail = new ExtendedPropertyDefinition(DefaultExtendedPropertySet.PublicStrings, "replyemail", MapiPropertyType.Boolean);
+
 
                 PropertySet set = new PropertySet(BasePropertySet.IdOnly, ItemSchema.Subject, ItemSchema.UniqueBody, ItemSchema.Attachments,
-             EmailMessageSchema.Body, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.DateTimeReceived, EmailMessageSchema.HasAttachments); //ItemSchema.TextBody,
+             EmailMessageSchema.Body, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.DateTimeReceived, EmailMessageSchema.HasAttachments,ItemSchema.InReplyTo, replyEmail); //ItemSchema.TextBody,
 
 
                 // Make an EWS call
-                SearchFilter foldername = new SearchFilter.IsEqualTo(FolderSchema.DisplayName, "FastaffTravelUpdates");
+                //    SearchFilter foldername = new SearchFilter.IsEqualTo(FolderSchema.DisplayName, "FastaffTravelUpdates");
+                //  SearchFilter subjectFilter = new SearchFilter.ContainsSubstring(ItemSchema.Subject, "[EXTERNAL] License nursys Jessica Welch (369580)");
+                //    var folders = service.FindFolders(WellKnownFolderName.MsgFolderRoot, foldername, new FolderView(100)).Result;
 
-            //    var folders = service.FindFolders(WellKnownFolderName.MsgFolderRoot, foldername, new FolderView(100)).Result;
                 SearchFilter time = new SearchFilter.IsLessThanOrEqualTo(ItemSchema.DateTimeReceived, DateTime.UtcNow);
-              //  SearchFilter subjectFilter = new SearchFilter.ContainsSubstring(ItemSchema.Subject, "[EXTERNAL] License nursys Jessica Welch (369580)");
+                SearchFilter reply = new SearchFilter.Exists(replyEmail);
+
+                   time = new SearchFilterCollection(LogicalOperator.Or, time, reply);
+
                 ItemView view = new ItemView(100);
 
-                //var findResults = service.FindItems(WellKnownFolderName.Inbox,time, view).Result;
+                var findResults = service.FindItems(new FolderId(WellKnownFolderName.Inbox, "compassa@thinkbridge.in"),time,view).Result;
 
-                // shared inbox mail access
-
-                FolderId SharedMailbox = new FolderId(WellKnownFolderName.Inbox, "compassa@thinkbridge.in");
-                ItemView itemView = new ItemView(100);
-                FindItemsResults<Item> findResults = service.FindItems(SharedMailbox, "", itemView).Result;
-
-                //  var findResults = folders.Folders.FirstOrDefault().FindItems(time, view).Result;
                 foreach (Item item in findResults)
                 {
+                    bool isReplyEmail = false;
                     var mail = item as EmailMessage;
                     var message = EmailMessage.Bind(service, item.Id, set).Result;
-
-                    var subject = message.Subject;
-                    var body = message.Body;
-                    var mailId = mail.Id.UniqueId;
-                    var from = message.From;
-                    var conversationId = item.ConversationId;
-                   
-                   
-                    // Request conversation items. This results in a call to the service.
-                    ConversationResponse response = service.GetConversationItems
-   (conversationId, set, null, null,
-   ConversationSortOrder.TreeOrderDescending).Result;
-
-
-                    foreach (ConversationNode node in response.ConversationNodes)
+                    object responseEmail;
+                    message.TryGetProperty(replyEmail, out responseEmail);
+                    if (responseEmail != null)
                     {
-                        foreach (Item nodeItem in node.Items)
+                      isReplyEmail   = (Boolean)responseEmail;
+
+                    }
+                    if (!isReplyEmail)
+                    {
+                        var subject = message.Subject;
+                        var recipient = message.ToRecipients;
+                        var body = message.Body;
+                        var mailId = mail.Id.UniqueId;
+                        var from = message.From;
+                        var conversationId = item.ConversationId;
+
+    
+
+
+                        // Request conversation items. This results in a call to the service.
+                        ConversationResponse response = service.GetConversationItems
+       (conversationId, set, null, null,
+       ConversationSortOrder.TreeOrderDescending).Result;
+
+
+                        foreach (ConversationNode node in response.ConversationNodes)
                         {
-                            var nodeConvItemBody = nodeItem.UniqueBody;
-
-                            var nodeConvItem_Subject= nodeItem.Subject;
-                        
-                            if (nodeItem.HasAttachments)
+                            foreach (Item nodeItem in node.Items)
                             {
-                                foreach (Microsoft.Exchange.WebServices.Data.Attachment attach in nodeItem.Attachments)
-                                {
-                                    FileAttachment fileAttachment = attach as FileAttachment;
-                                    if(fileAttachment != null)
-                                    {
-                                        fileAttachment.Load().Wait();
-                                        if (new List<string> { "pdf", "doc", "docx", "odt", "odtx", "rtf", "txt", "jpeg", "png", "jpg" }.Contains(fileAttachment.Name.Split('.').Last().ToLower()))
-                                        {
-                                            var memoryStream = new System.IO.MemoryStream(fileAttachment.Content);
-                                            memoryStream.Seek(0, SeekOrigin.Begin);
-                                            memoryStream.ToArray();
-                                            var fileCreationInfo = new FileCreationInformation
-                                            {
-                                                //Content = fileAttachment.Content.ToArray(),
-                                                Overwrite = true,
-                                                Url = fileAttachment.Name,
-                                                ContentStream =memoryStream
-                                            };
-                                           // var img = SaveFileToBlobStorage(fileAttachment.Name, fileAttachment.ContentType, fileAttachment.Content, "", "");
-                                            var url = UploaToSharepoint(fileCreationInfo, mailId);
+                                var nodeConvItemBody = nodeItem.UniqueBody;
 
+                                var nodeConvItem_Subject = nodeItem.Subject;
+
+                                if (nodeItem.HasAttachments)
+                                {
+                                    foreach (Microsoft.Exchange.WebServices.Data.Attachment attach in nodeItem.Attachments)
+                                    {
+                                        FileAttachment fileAttachment = attach as FileAttachment;
+                                        if (fileAttachment != null)
+                                        {
+                                            fileAttachment.Load().Wait();
+                                            if (new List<string> { "pdf", "doc", "docx", "odt", "odtx", "rtf", "txt", "jpeg", "png", "jpg" }.Contains(fileAttachment.Name.Split('.').Last().ToLower()))
+                                            {
+                                                var memoryStream = new System.IO.MemoryStream(fileAttachment.Content);
+                                                memoryStream.Seek(0, SeekOrigin.Begin);
+                                                memoryStream.ToArray();
+                                                var fileCreationInfo = new FileCreationInformation
+                                                {
+                                                    //Content = fileAttachment.Content.ToArray(),
+                                                    Overwrite = true,
+                                                    Url = fileAttachment.Name,
+                                                    ContentStream = memoryStream
+                                                };
+                                                // var img = SaveFileToBlobStorage(fileAttachment.Name, fileAttachment.ContentType, fileAttachment.Content, "", "");
+                                                var url = UploaToSharepoint(fileCreationInfo, mailId);
+
+                                            }
                                         }
+
                                     }
-                                 
                                 }
                             }
                         }
-                    }
-                  
-                  //  await message.Reply( "Reply for test.",false);
-                    #region this code belongs to attachments not received in chains.
-                    //if (message.HasAttachments)
-                    //{
-                    //    foreach (var att in message.Attachments)
-                    //    {
-                    //        if (att is FileAttachment) //also probably need to add a check for content type as there could be images as part of attachments
-                    //        {
-                    //            var file = (att as FileAttachment);
-                    //            file.Load().Wait();
-                    //            if (new List<string> { "pdf", "doc", "docx", "odt", "odtx", "rtf", "txt","jpeg","png","jpg" }.Contains(file.Name.Split('.').Last().ToLower()))
-                    //            {
-                    //               var img =  SaveFileToBlobStorage(file.Name, file.ContentType, file.Content, "", "");
 
-                    //            }
-                    //        }
-                    //    }
-                    //}
-                    #endregion
+                        //  await message.Reply( "Reply for test.",false);
+                        #region this code belongs to attachments not received in chains.
+                        //if (message.HasAttachments)
+                        //{
+                        //    foreach (var att in message.Attachments)
+                        //    {
+                        //        if (att is FileAttachment) //also probably need to add a check for content type as there could be images as part of attachments
+                        //        {
+                        //            var file = (att as FileAttachment);
+                        //            file.Load().Wait();
+                        //            if (new List<string> { "pdf", "doc", "docx", "odt", "odtx", "rtf", "txt","jpeg","png","jpg" }.Contains(file.Name.Split('.').Last().ToLower()))
+                        //            {
+                        //               var img =  SaveFileToBlobStorage(file.Name, file.ContentType, file.Content, "", "");
+
+                        //            }
+                        //        }
+                        //    }
+                        //}
+                        #endregion
+                    }
+
                 }
 
             }
@@ -175,34 +190,99 @@ namespace OutlookMailParser_POC
             }
         public static async void ReplytoSharedInboxEmail()
         {
-            var mailId = "AQMkADcwYmE1ZjFlLTFkNGMtNGUzMC04ZTA5LWU5YWY2MmIzYTI0MABGAAADfpF1NE3FOECC8Qouth0+OQcAWdx5YeH+FUegFE2qtIz+UwAAAgEMAAAAWdx5YeH+FUegFE2qtIz+UwAAAgVbAAAA";
+
+                        var mailId = "AAMkADcwYmE1ZjFlLTFkNGMtNGUzMC04ZTA5LWU5YWY2MmIzYTI0MABGAAAAAAB+kXU0TcU4QILxCi62HT45BwBZ3Hlh4f4VR6AUTaq0jP5TAAAAAAEMAABZ3Hlh4f4VR6AUTaq0jP5TAAAHEdOSAAA=";
             PropertySet set = new PropertySet(BasePropertySet.IdOnly, ItemSchema.Subject, ItemSchema.UniqueBody, ItemSchema.Attachments,
-         EmailMessageSchema.Body, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.DateTimeReceived, EmailMessageSchema.HasAttachments); //ItemSchema.TextBody,
+         EmailMessageSchema.Body, EmailMessageSchema.From, EmailMessageSchema.ToRecipients, EmailMessageSchema.DateTimeReceived, EmailMessageSchema.HasAttachments,EmailMessageSchema.ReplyTo,EmailMessageSchema.Sender); //ItemSchema.TextBody,
 
             EmailMessage msg = (EmailMessage)Item.Bind(service, new ItemId(mailId), set).Result;
             var subject = msg.Subject;
-            var rec = msg.From;
-            ResponseMessage responseMessage = msg.CreateReply(false);
-        
-            string myReply = "This is the message body of the email reply.";
-            responseMessage.BodyPrefix = myReply;
-           
-            responseMessage.IsDeliveryReceiptRequested = true;
-            responseMessage.ToRecipients.Add("priyanka@thinkbridge.in");
 
-            // Send the response message.
-            // This method call results in a CreateItem call to EWS.
+            //  msg.ToRecipients.Add("abhishek@thinkbridge.in");
+            //  msg.Body = new MessageBody("test sample.");
+            //ExtendedPropertyDefinition PidTagReplyRecipientEntries = new ExtendedPropertyDefinition(0x004F, MapiPropertyType.Binary);
+            //ExtendedPropertyDefinition PidTagReplyRecipientNames = new ExtendedPropertyDefinition(0x0050, MapiPropertyType.String);
+            // msg.SetExtendedProperty(PidTagReplyRecipientEntries, ConvertHexStringToByteArray(GenerateFlatList("swati@thinkbridge.in", "Swati K")));
+            //msg.InReplyTo = "replymail";
+            //msg.SetExtendedProperty(PidTagReplyRecipientNames, "Swati K");
+
+            ExtendedPropertyDefinition replyEmail =  new ExtendedPropertyDefinition(DefaultExtendedPropertySet.PublicStrings, "replyemail", MapiPropertyType.Boolean);
+          //  msg.SetExtendedProperty(replyEmail,true);
+            //  await msg.Reply(new MessageBody { BodyType = BodyType.Text, Text = "This sample text on date 4th OCT. Test 4. 20:46" },true);
+
+            ResponseMessage responseMessage = msg.CreateReply(true);
+            msg.From = new EmailAddress { Address = "swati@thinkbridge.in", Name = "Swati K" };
+            string myReply = "This is the message body of the email reply.21:20";
+            responseMessage.BodyPrefix = myReply;
+        
+            // // Send the response message.
+            // // This method call results in a CreateItem call to EWS.
             try
             {
-                await responseMessage.SendAndSaveCopy();
+                // to set replyemail prop true in reply mail so it will exclude from creating the task in next iterations
+                var  e =  await responseMessage.Save();
+                e.SetExtendedProperty(replyEmail, true);
+                await e.Send();
+            
 
             }
-            catch (Exception ex)
+            catch (System.Exception ex)
             {
 
             }
 
         }
+
+
+
+  
+
+    internal static String GenerateFlatList(String SMTPAddress, String DisplayName)
+        {
+            String abCount = "01000000";
+            String AddressId = GenerateOneOff(SMTPAddress, DisplayName);
+            return abCount + BitConverter.ToString(INT2LE((AddressId.Length / 2) + 4)).Replace("-", "") + BitConverter.ToString(INT2LE(AddressId.Length / 2)).Replace("-", "") + AddressId;
+        }
+
+        internal static String GenerateOneOff(String SMTPAddress, String DisplayName)
+        {
+            String Flags = "00000000";
+            String ProviderUid = "812B1FA4BEA310199D6E00DD010F5402";
+            String Version = "0000";
+            String xFlags = "0190";
+            String DisplayNameHex = BitConverter.ToString(UnicodeEncoding.Unicode.GetBytes(DisplayName + "\0")).Replace("-", "");
+            String SMTPAddressHex = BitConverter.ToString(UnicodeEncoding.Unicode.GetBytes(SMTPAddress + "\0")).Replace("-", "");
+            String AddressType = BitConverter.ToString(UnicodeEncoding.Unicode.GetBytes("SMTP" + "\0")).Replace("-", "");
+            return Flags + ProviderUid + Version + xFlags + DisplayNameHex + AddressType + SMTPAddressHex;
+        }
+        internal static byte[] INT2LE(int data)
+        {
+            byte[] b = new byte[4];
+            b[0] = (byte)data;
+            b[1] = (byte)(((uint)data >> 8) & 0xFF);
+            b[2] = (byte)(((uint)data >> 16) & 0xFF);
+            b[3] = (byte)(((uint)data >> 24) & 0xFF);
+            return b;
+        }
+        internal static byte[] ConvertHexStringToByteArray(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(String.Format(CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] HexAsBytes = new byte[hexString.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
+
+        }
+
+
 
         public static string SaveFileToBlobStorage(string fileName, string contentType, byte[] content, string storageContainer, string contentDisposition = "")
         {
